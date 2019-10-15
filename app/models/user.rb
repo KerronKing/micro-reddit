262444bf4153b265c968e55b4459d4183daf2755
KerronKing <king_of_palmiste@hotmail.com>
2019-10-15 11:36:34 -0400

    Wrote validations and created a new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :name, 
+    presence: true, 
+    length: { maximum: 24 }
+
+  validates :email, 
+    presence: true, 
+    length: { maximum: 255 }
 end
